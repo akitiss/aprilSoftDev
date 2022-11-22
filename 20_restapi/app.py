1714e9586039f3a40e36784b6407896ca607cb95
April Li <april.li0427@gmail.com>
2022-11-21 22:46:57 -0500

    :)

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -2,20 +2,25 @@
 # SoftDev pd08
 # K20: Restful API 
 # 2022-11-21
-# time spent: 
+# time spent: 0.5
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+import requests                     #facilitate http requests
 
 app = Flask(__name__)    #create Flask object
 
 key_file = open("key_nasa.txt", "r") 
 key = key_file.read()
 
-@app.route('/', methods = ['GET', 'POST'])
+get_the_thing = "https://api.nasa.gov/planetary/apod?api_key=" + key
+the_thing = requests.get(get_the_thing)
+thing_formatted = the_thing.json()
+# print(thing_formatted)
+
+@app.route('/')
 def open_api():
-    return render_template("main.html")
+    return render_template("main.html", title = thing_formatted['title'], explanation = thing_formatted['explanation'], url = thing_formatted['url'])
 
 
 if __name__ == "__main__": #false if this file imported as module
